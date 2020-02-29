2091f3f8b45541fcc05f15a3a5f5803608adfebd
Sergey <bashkin2013@mail.ru>
2020-02-29 10:50:36 +0400

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+  if ((!Array.isArray(array)) || (array.length == 0)) {
+      return 0;
+  }
+  array = array.sort();
+  if (array[0] < 0) {
+      let min = array[0];
+      array.map(el => {
+          if (el >= 0) {
+              return min;
+          }
+          if (el < min) {
+              min = el;
+          }
+      });
+      return min
+  } else {
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((!Array.isArray(array)) || (array.length == 0)) {
+        return 0;
+    }
+    let max = array[0];
+    array.map(el => {
+        if (max < el) {
+            max = el;
+        }
+    })
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((!Array.isArray(array)) || (array.length == 0)) {
+        return 0;
+    }
+    let avg = 0;
+    array.map(el => {
+        avg += el;
+    })
+    return avg / array.length;
 }
